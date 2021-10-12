b58b8cebcf3870421c32c032fc30c8fa547b708f
ishrakhussain <ishrak.hossen8@gmail.com>
2021-10-12 16:36:28 -0400

    Bismillah

---
+++ malta/src/malta/malta.java
@@ -0,0 +1,24 @@
+package malta;
+
+public class malta {
+int a=2;
+double b=3;
+short c=32000;
+long d=890290392;
+char e=65000;
+boolean p=1<2;
+	
+	
+	
+public static void main(String[] args) {
+
+
+
+	
+	
+	malta1 qA = new malta1 ();
+qA.sweet();
+
+}
+
+}
---
+++ malta/src/malta/malta1.java
@@ -0,0 +1,14 @@
+package malta;
+
+public class malta1 {
+	
+
+public void sweet ( ) {	
+	
+System.out.println("I am learning");
+System.out.println ("Sraboni is learing java");	
+	
+	
+	
+}
+}
